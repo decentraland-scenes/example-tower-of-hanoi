c571e28ef19e3d763b176df4f36bbca261787954
vrglitch <64665120+vrglitch@users.noreply.github.com>
2024-07-11 16:17:20 -0300

    win Animation

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { initGame } from "./game";
 import { Color4, Quaternion, Vector3 } from '@dcl/sdk/math'
-import { ColliderLayer, EasingFunction, Entity, GltfContainer, InputAction, Material, MeshCollider, MeshRenderer, PBMeshCollider, PointerEvents, Schemas, TextShape, Transform, TransformType, Tween, TweenLoop, TweenSequence, engine, pointerEventsSystem, tweenSystem } from '@dcl/sdk/ecs'
+import { ColliderLayer, EasingFunction, Entity, GltfContainer, Billboard, InputAction, Material, MeshCollider, MeshRenderer, PBMeshCollider, PointerEvents, Schemas, TextShape, Transform, TransformType, Tween, TweenLoop, TweenSequence, engine, pointerEventsSystem, tweenSystem } from '@dcl/sdk/ecs'
 
 export function main() {
     let backSign = engine.addEntity()
@@ -91,6 +91,72 @@ export function main() {
         rotation: Quaternion.fromEulerDegrees(0, -90, 0)
     })
 
+    let winAnimA = engine.addEntity()
 
+    GltfContainer.create(winAnimA, {
+        src: "assets/scene/winAnim.glb"
+
+    })
+
+    Transform.create(winAnimA, {
+        position: Vector3.create(14, 0.2, 2),
+        scale: Vector3.create(1, 1, 1),
+        rotation: Quaternion.fromEulerDegrees(0,45, 0)
+    })
+
+    let winAnimB = engine.addEntity()
+
+    GltfContainer.create(winAnimB, {
+        src: "assets/scene/winAnim.glb"
+
+    })
+
+    Transform.create(winAnimB, {
+        position: Vector3.create(14, 0.2, 8),
+        scale: Vector3.create(1, 1, 1),
+        rotation: Quaternion.fromEulerDegrees(0,0, 0)
+    })
+
+    let winAnimC = engine.addEntity()
+
+    GltfContainer.create(winAnimC, {
+        src: "assets/scene/winAnim.glb"
+
+    })
+
+    Transform.create(winAnimC, {
+        position: Vector3.create(14, 0.2, 14),
+        scale: Vector3.create(1, 1, 1),
+        rotation: Quaternion.fromEulerDegrees(0,-45, 0)
+    })
+
+    let winAnimFollow = engine.addEntity()
+
+    GltfContainer.create(winAnimFollow, {
+        src: "assets/scene/winAnimFollow.glb"
+
+    })
+
+    Transform.create(winAnimFollow, {
+        position: Vector3.create(10, 2, 8),
+        scale: Vector3.create(0.3, 0.3, 0.3),
+        rotation: Quaternion.fromEulerDegrees(0,-90, 0)
+    })
+    Billboard.create(winAnimFollow, {})
+
+    
+    let winAnimText = engine.addEntity()
+
+    GltfContainer.create(winAnimText, {
+        src: "assets/scene/winAnimText.glb"
+
+    })
+
+    Transform.create(winAnimText, {
+        position: Vector3.create(10, 2, 8),
+        scale: Vector3.create(0.8, 0.8, 0.8),
+        rotation: Quaternion.fromEulerDegrees(0,-90, 0)
+    })
+    Billboard.create(winAnimText, {})
     initGame()
 }
