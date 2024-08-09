8fb42d01ce070beb2b652ab934a9053f4993c2d3
Gonzalo DCL <gonzalo@decentraland.org>
2024-08-08 22:48:56 -0300

    update mini-games

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,14 @@ import { syncEntity } from "@dcl/sdk/network";
 import playersApi from '@dcl/sdk/players'
 import * as miniGames from "@dcl-sdk/mini-games/src"
 
+const _1_SEC = 1000
+const _1_MIN = _1_SEC * 60
+
 miniGames.initLibrary(engine as any, syncEntity, playersApi, {
     gameId: "4ee1d308-5e1e-4b2b-9e91-9091878a7e3d",
     environment: "dev",
     // 20 seconds (testing purpose)
-    gameTimeoutMs: 20000
+    gameTimeoutMs: _1_MIN
 })
 
 export function main() {
